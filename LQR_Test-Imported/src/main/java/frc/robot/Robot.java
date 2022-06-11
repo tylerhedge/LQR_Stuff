0dbfa8aff4399b324893a1c2e74d6661c6716cb1
rishik4 <34404515+tylerhedge@users.noreply.github.com>
2022-06-11 11:06:10 -0500

    tuning controller

--- LQR_Test-Imported/src/main/java/frc/robot/Robot.java
+++ LQR_Test-Imported/src/main/java/frc/robot/Robot.java
@@ -80,10 +80,10 @@ public class Robot extends TimedRobot {
   private final LinearQuadraticRegulator<N1, N1, N1> m_controller =
       new LinearQuadraticRegulator<>(
           m_flywheelPlant,
-          VecBuilder.fill(20), // qelms. Velocity error tolerance, in radians per second. Decrease
+          VecBuilder.fill(0.1), // qelms. Velocity error tolerance, in radians per second. Decrease
           // this to more heavily penalize state excursion, or make the controller behave more
           // aggressively.
-          VecBuilder.fill(6), // relms. Control effort (voltage) tolerance. Decrease this to more
+          VecBuilder.fill(12), // relms. Control effort (voltage) tolerance. Decrease this to more
           // heavily penalize control effort, or make the controller less aggressive. 12 is a good
           // starting point because that is the (approximate) maximum voltage of a battery.
           0.020); // Nominal time between loops. 0.020 for TimedRobot, but can be
